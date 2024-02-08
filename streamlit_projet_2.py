ddaee55309f0725f5dc5ed8a329fdd281a7c9753
The-Pandwa <costinblanc@gmail.com>
2024-02-08 03:55:14 +0100

    test

--- streamlit_projet_2.py
+++ streamlit_projet_2.py
@@ -77,8 +77,25 @@ citations_films = [
     "Vous savez, moi je ne crois pas qu’il y ait de bonne ou de mauvaise situation. Moi, si je devais résumer ma vie aujourd’hui avec vous, je dirais que c’est d’abord des rencontres. Des gens qui m’ont tendu la main, peut-être à un moment où je ne pouvais pas, où j’étais seul chez moi. Et c’est assez curieux de se dire que les hasards, les rencontres forgent une destinée... Parce que quand on a le goût de la chose, quand on a le goût de la chose bien faite, le beau geste, parfois on ne trouve pas l’interlocuteur en face je dirais, le miroir qui vous aide à avancer. Alors ça n’est pas mon cas, comme je disais là, puisque moi au contraire, j’ai pu ; et je dis merci à la vie, je lui dis merci, je chante la vie, je danse la vie... je ne suis qu’amour ! Et finalement, quand des gens me disent « Mais comment fais-tu pour avoir cette humanité ? », je leur réponds très simplement que c’est ce goût de l’amour, ce goût donc qui m’a poussé aujourd’hui à entreprendre une construction mécanique... mais demain qui sait ? Peut-être simplement à me mettre au service de la communauté, à faire le don, le don de soi. - Astérix et Obélix : Mission Cléopâtre (2002)"
 ]
 
+phrase_chargement = [
+    "Plonger dans la matrice en cours"
+    "En attente de l'arrivée des dinosaures"
+    "Chargement dans l'espace-temps, façon Retour vers le Futur."
+    "Élaboration de votre Truman Show personnel"
+    "Tentative d'éviter l'iceberg droit devant !"
+    "Saut dans l'hyperespace"
+    "Auto-destruction de l'écran de chargement en cours..."
+    "Préparation de votre rêve partagé"
+    "Ouverture d'une brèche dans votre chargement à coup de hache"
+    "C'est un écran, un écran de chargement "
+    "Déchiffrement du Da Vinci Code en cours..."
+    "Recrutement de nouveau Avengers en cours "
+    "Concoction de votre potion cinématographique en cours."
+    "Création de la communauté des cinéphiles"
+]
+
 # Sélectionner une citation aléatoire
-citation_aleatoire = random.choice(citations_films)
+citation_aleatoire = random.choice(phrase_chargement)
 
 with st.spinner(citation_aleatoire):
     time.sleep(5)
